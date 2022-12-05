0eeaad85aa9cdb92a2d6bcef29a0c25c70067486
Andrew Prinn <andrewprinn@gmail.com>
2022-12-05 13:54:36 -0500

    render gameboard array to the webpage

--- script.js
+++ script.js
@@ -4,22 +4,37 @@
 //If you need multiples of something (players!), create them with factories.
 
 //You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
-//Gamebaord object 
+//Gameboard object 
     //gameboard array 
-//need one; use module
+//need one of this, so use a module
+const gameboardObject = (() => {
+    let gameboardArray = ["o", "x", "o", "x", "", "", "o", "x", "o"];
+    return {gameboardArray}
+})();
 
 
 //Your players are also going to be stored in objects
 //player object
-//need multiple; use factory
-
+//need multiple of these, so use a factory
+function Player(name) {
+    const getName = () => name;
+    return { getName };
+}
 
 //you’re probably going to want an object to control the flow of the game itself.
-//gameflow
 //
+
 
 //function that will render the contents 
 //of the gameboard array to the webpage
+const displayController = (() => {
+    let gamespaces = document.querySelectorAll(".gamespace");
+    for (i = 0; i < 9; i++){
+        console.log(gameboardObject.gameboardArray[i]);
+        gamespaces[i].innerHTML = gameboardObject.gameboardArray[i];
+    }
+})();
+
 
 //Build the functions that allow players to 
 //add marks to a specific spot on the board, and then 
