45db170bbd0fb4ac5a796c34eb97eebb2421eb0c
Andrew Prinn <andrewprinn@gmail.com>
2022-12-05 22:59:21 -0500

    scoring system declares winner

--- script.js
+++ script.js
@@ -25,88 +25,57 @@ const displayController = (() => {
                 gameboardObject.gameboardArray[1] && 
                 gameboardObject.gameboardArray[1] == 
                 gameboardObject.gameboardArray[2]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[3] == 
                 gameboardObject.gameboardArray[4] && 
                 gameboardObject.gameboardArray[4] == 
                 gameboardObject.gameboardArray[5]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[6] == 
                 gameboardObject.gameboardArray[7] && 
                 gameboardObject.gameboardArray[7] == 
                 gameboardObject.gameboardArray[8]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[0] == 
                 gameboardObject.gameboardArray[3] && 
                 gameboardObject.gameboardArray[3] == 
                 gameboardObject.gameboardArray[6]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[1] == 
                 gameboardObject.gameboardArray[4] && 
                 gameboardObject.gameboardArray[4] == 
                 gameboardObject.gameboardArray[7]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[2] == 
                 gameboardObject.gameboardArray[5] && 
                 gameboardObject.gameboardArray[5] == 
                 gameboardObject.gameboardArray[8]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[0] == 
                 gameboardObject.gameboardArray[4] && 
                 gameboardObject.gameboardArray[4] == 
                 gameboardObject.gameboardArray[8]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
                 gameboardObject.gameboardArray[2] == 
                 gameboardObject.gameboardArray[4] && 
                 gameboardObject.gameboardArray[4] == 
                 gameboardObject.gameboardArray[6]){
-                document.getElementById('result').innerHTML = "game over";
+                document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
         }
     }
 })();
-
-//Your players are also going to be stored in objects
-//player object
-//need multiple of these, so use a factory
-function Player(name) {
-    const getName = () => name;
-    return { getName };
-}
-
-//you’re probably going to want an object to control the flow of the game itself.
-
-
-//Build the functions that allow players to 
-//add marks to a specific spot on the board, and then 
-//tie it to the DOM, letting players click on the gameboard 
-//to place their marker. Don’t forget the logic that keeps 
-//players from playing in spots that are already taken!
-//Think carefully about where each bit of logic 
-//should reside. Each little piece of functionality 
-//should be able to fit in the game, player or gameboard 
-//objects.. but take care to put them in “logical” places.
-// Spending a little time brainstorming here can make your 
-//life much easier later!
-
-//Build the logic that checks for when the game is over! Should check for 3-in-a-row and a tie.
-
-//Clean up the interface to allow players to put in their names, 
-//include a button to start/restart the game and add a display
-// element that congratulates the winning player!
-
-
