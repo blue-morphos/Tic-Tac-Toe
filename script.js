aab667a0e1d5ad2ae1cee3230d24c38e7e261a47
Andrew Prinn <andrewprinn@gmail.com>
2022-12-05 19:30:28 -0500

    pushMarkerToGameboardArray() works

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-//Your main goal here is to have as little global code as possible. 
-//Try tucking everything away inside of a module or factory. 
-//Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. 
-//If you need multiples of something (players!), create them with factories.
-
-//You’re going to store the gameboard as an array inside of a Gameboard object, so start there!
-//Gameboard object 
-    //gameboard array 
-//need one of this, so use a module
 const gameboardObject = (() => {
     let turn = 1;
     let gameboardArray = ["", "", "", "", "", "", "", "", ""];
     const displayController = (() => {
         let gamespaces = document.querySelectorAll(".gamespace");
         for (i = 0; i < 9; i++){
-            console.log(gameboardArray[i]);
-            gamespaces[i].innerHTML = gameboardArray[i];
             gamespaces[i].addEventListener('click', addToArray);
             function addToArray(e){
                 if (turn %2 != 0){
@@ -23,12 +12,12 @@ const gameboardObject = (() => {
                 if (turn %2 == 0){
                     marker = "o"
                 }
-                gameboardObject.gameboardArray.shift();
-                gameboardObject.gameboardArray.push(marker);
-               console.log(gameboardObject.gameboardArray);
                 e.target.innerHTML= marker;
+               // gameboardArray.push(e.target.innerHTML);
                 turn++;
-                console.log(turn);
+            }
+            for (j = 0; j < gameboardArray.length; j++){
+                gamespaces[j].innerHTML = gameboardArray[j];
             }
         }
     })();
@@ -45,9 +34,7 @@ function Player(name) {
 }
 
 //you’re probably going to want an object to control the flow of the game itself.
-const game = (() => {
 
-})();
 
 //Build the functions that allow players to 
 //add marks to a specific spot on the board, and then 
@@ -66,3 +53,18 @@ const game = (() => {
 //Clean up the interface to allow players to put in their names, 
 //include a button to start/restart the game and add a display
 // element that congratulates the winning player!
+
+
+const spaces = document.querySelectorAll('.gamespace');
+let $location = [];
+for (let i = 0; i < spaces.length; i++){
+    const space = spaces[i];
+    space.addEventListener('click', pushMarkerToGameboardArray);
+    $location=i;
+    function pushMarkerToGameboardArray(){
+        gameboardObject.gameboardArray[i]=marker;
+        console.log(gameboardObject.gameboardArray);
+        console.log([i]);
+       
+    }
+}
