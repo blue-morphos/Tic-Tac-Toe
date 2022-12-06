d5a2d74a0f94d528c53647de434630a6dcaf7bcf
Andrew Prinn <andrewprinn@gmail.com>
2022-12-05 23:41:58 -0500

    moved scoring system into gameboardObject module

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const gameboardObject = (() => {
     let gameboardArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-    return {gameboardArray};
-})();
-
-const displayController = (() => {
     let turn = 1;
     let gamespaces = document.querySelectorAll(".gamespace");
     for (let i = 0; i < 9; i++){
@@ -17,63 +12,63 @@ const displayController = (() => {
                 marker = "o"
             }
             e.target.innerHTML= marker;
-            gameboardObject.gameboardArray[i]=marker;
+            gameboardArray[i]=marker;
             turn++;
             console.log(turn)
             if (turn > 4 &&
-                gameboardObject.gameboardArray[0] == 
-                gameboardObject.gameboardArray[1] && 
-                gameboardObject.gameboardArray[1] == 
-                gameboardObject.gameboardArray[2]){
+                gameboardArray[0] == 
+                gameboardArray[1] && 
+                gameboardArray[1] == 
+                gameboardArray[2]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[3] == 
-                gameboardObject.gameboardArray[4] && 
-                gameboardObject.gameboardArray[4] == 
-                gameboardObject.gameboardArray[5]){
+                gameboardArray[3] == 
+                gameboardArray[4] && 
+                gameboardArray[4] == 
+                gameboardArray[5]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[6] == 
-                gameboardObject.gameboardArray[7] && 
-                gameboardObject.gameboardArray[7] == 
-                gameboardObject.gameboardArray[8]){
+                gameboardArray[6] == 
+                gameboardArray[7] && 
+                gameboardArray[7] == 
+                gameboardArray[8]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[0] == 
-                gameboardObject.gameboardArray[3] && 
-                gameboardObject.gameboardArray[3] == 
-                gameboardObject.gameboardArray[6]){
+                gameboardArray[0] == 
+                gameboardArray[3] && 
+                gameboardArray[3] == 
+                gameboardArray[6]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[1] == 
-                gameboardObject.gameboardArray[4] && 
-                gameboardObject.gameboardArray[4] == 
-                gameboardObject.gameboardArray[7]){
+                gameboardArray[1] == 
+                gameboardArray[4] && 
+                gameboardArray[4] == 
+                gameboardArray[7]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[2] == 
-                gameboardObject.gameboardArray[5] && 
-                gameboardObject.gameboardArray[5] == 
-                gameboardObject.gameboardArray[8]){
+                gameboardArray[2] == 
+                gameboardArray[5] && 
+                gameboardArray[5] == 
+                gameboardArray[8]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[0] == 
-                gameboardObject.gameboardArray[4] && 
-                gameboardObject.gameboardArray[4] == 
-                gameboardObject.gameboardArray[8]){
+                gameboardArray[0] == 
+                gameboardArray[4] && 
+                gameboardArray[4] == 
+                gameboardArray[8]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn > 4 &&
-                gameboardObject.gameboardArray[2] == 
-                gameboardObject.gameboardArray[4] && 
-                gameboardObject.gameboardArray[4] == 
-                gameboardObject.gameboardArray[6]){
+                gameboardArray[2] == 
+                gameboardArray[4] && 
+                gameboardArray[4] == 
+                gameboardArray[6]){
                 document.getElementById('result').innerHTML = "game over. Winner: " + marker;
             }
             if (turn === 10){
@@ -81,4 +76,10 @@ const displayController = (() => {
             }
         }
     }
+
+    return {gameboardArray};
+})();
+
+const displayController = (() => {
+
 })();
